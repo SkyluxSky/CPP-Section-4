bee9a50032b14c4dec9161795ace67b6b57e327a
Cameron <44791770+SkyluxSky@users.noreply.github.com>
2019-07-14 17:46:01 -0400

    Add files via upload

---
+++ Section4/ChallengeSection4/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+/*Challenge Section 4*/
+
+int main() {
+    int favorite_number; 
+    std::cout << "Enter your favorite number between 1 and 100:" << std::endl;
+    std::cin >> favorite_number;
+    std::cout << "Amazing! That is my favorite number too!" << std::endl;
+    std::cout << "No really!!, " << favorite_number << " is my favorite number!" << std::endl;
+    return 0;
+}
+
+/*#include <iostream>
+int main() {
+    int favorite_number; 
+    std::cout << "Enter your favorite number between 1 and 100:" << std::endl;
+    std::cin >> favorite_number;
+    std::cout << "Amazing! That is my favorite number too!" << std::endl;
+    std::cout << "No really!!, " << favorite_number << "is my favorite number! <<std::endl;*/
